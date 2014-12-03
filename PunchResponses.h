4e0695e11545420dfe2b363b75e9f9d7c3f63cc5
Elecia White <eleciawhite@users.noreply.github.com>
2014-12-03 13:30:18 -0800

    Create PunchResponses.h

---
+++ PunchResponses.h
@@ -0,0 +1,26 @@
+
+char punch_1[] PROGMEM = "  BAM!";
+char punch_2[] PROGMEM = "  BIF";
+char punch_3[] PROGMEM = "  POW";
+char punch_4[] PROGMEM = "Zowee!!";
+char punch_5[] PROGMEM = " Kapow";
+char punch_6[] PROGMEM = " SPLATT";
+char punch_7[] PROGMEM = "Zoinks!";
+
+
+PROGMEM const char* gPunchSoundsList[] = {
+	punch_1,
+	punch_2,
+	punch_3,
+	punch_4,
+	punch_5,
+	punch_6,
+	punch_7,
+};
+
+const int gPunchSoundsListLength = sizeof(gPunchSoundsList)/sizeof(gPunchSoundsList[0]);
+
+
+
+
+
