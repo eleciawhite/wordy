ac8c5799db23dd8a3f62cce679f2343cac19d3ab
Elecia White <eleciawhite@users.noreply.github.com>
2014-12-03 13:29:58 -0800

    Create MagicAnswers.h

---
+++ MagicAnswers.h
@@ -0,0 +1,53 @@
+char gMagicQuestion[] PROGMEM = " Consider\n   Your\n Question!";
+
+char answer_1[] PROGMEM = "It is\n certain";
+char answer_2[] PROGMEM = "It is\n decidedly   so";
+char answer_3[] PROGMEM = "Without\n   a\n doubt";
+char answer_4[] PROGMEM = "  Yes\ndefinitely";
+char answer_5[] PROGMEM = "You may \nrely on it";
+char answer_6[] PROGMEM = "  As I\n see it,\n   yes";
+char answer_7[] PROGMEM = "Most\nlikely";
+char answer_8[] PROGMEM = "Outlook\ngood";
+char answer_9[] PROGMEM = "Yes";
+char answer_10[] PROGMEM = "Signs\npoint to\n yes";
+char answer_11[] PROGMEM = "Reply hazy try again";
+char answer_12[] PROGMEM = "Ask again\n later";
+char answer_13[] PROGMEM = "Better not tell you now";
+char answer_14[] PROGMEM = "Cannot \npredict\n now";
+char answer_15[] PROGMEM = "Concen-\ntrate\n and\n ask again";
+char answer_16[] PROGMEM = "Don't\ncount\non it";
+char answer_17[] PROGMEM = "My reply\n is no";
+char answer_18[] PROGMEM = "My sources  say no";
+char answer_19[] PROGMEM = "Outlook\n not \n so good";
+char answer_20[] PROGMEM = "Very \n doubtful";
+
+
+PROGMEM const char* gMagicAnswerList[] = {
+	answer_1,
+	answer_2,
+	answer_3,
+	answer_4,
+	answer_5,
+	answer_6,
+	answer_7,
+	answer_8,
+	answer_9,
+	answer_10,
+	answer_11,
+	answer_12,
+	answer_13,
+	answer_14,
+	answer_15,
+	answer_16,
+	answer_17,
+	answer_18,
+	answer_19,
+	answer_20,
+};
+
+const int gMagicAnswerListLength = sizeof(gMagicAnswerList)/sizeof(gMagicAnswerList[0]);
+
+
+
+
+
